43da5e4d45b57755e82a3d335232ab73404c0182
Sergei Lapukhin <yasenya1985@yandex.ru>
2023-10-09 21:33:39 +0300

    add modal window

--- src/main/java/com/newnrg/springjavafxclient/JavafxClientNewNRG.java
+++ src/main/java/com/newnrg/springjavafxclient/JavafxClientNewNRG.java
@@ -5,7 +5,6 @@ import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxWeaver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -14,10 +13,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class JavafxClientNewNRG extends Application {
 
     private ConfigurableApplicationContext context;
-    private boolean isUserLoggedIn = false;
-    private FxWeaver fxWeaver = context.getBean(FxWeaver.class);
-    private Stage stageMain;
-    private Stage stageAuthorization;
 
     @Override
     public void init() {
@@ -35,26 +30,12 @@ public class JavafxClientNewNRG extends Application {
 
     @Override
     public void start(Stage stageMain) throws Exception {
-        this.stageMain = stageMain;
-        initMainWindow();
-        initAuthorizationWindow();
-    }
-
-    private void initMainWindow() {
+        FxWeaver fxWeaver = context.getBean(FxWeaver.class);
         Parent root = fxWeaver.loadView(MainController.class);
         Scene scene = new Scene(root);
         stageMain.setScene(scene);
         stageMain.setTitle("НОВАЯ ЭНЕРГИЯ");
         stageMain.getIcons().add(new Image("file:icon.png"));
         stageMain.show();
-    }
-
-    private void initAuthorizationWindow() {
-        stageAuthorization = new Stage();
-        stageAuthorization.initModality(Modality.APPLICATION_MODAL);
-        Parent root = fxWeaver.loadView(AuthorizationController.class);
-        Scene scene = new Scene(root);
-        stageAuthorization.setScene(scene);
-        stageAuthorization.show();
     }
 }
--- src/main/java/com/newnrg/springjavafxclient/MainController.java
+++ src/main/java/com/newnrg/springjavafxclient/MainController.java
@@ -1,9 +1,32 @@
 package com.newnrg.springjavafxclient;
 
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Component
 @FxmlView("main-controller.fxml")
-public class MainController {
+public class MainController implements Initializable {
+
+
+    private boolean isUserLoggedIn = false;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        Stage stageAuthorization = new Stage();
+//        stageAuthorization.initModality(Modality.APPLICATION_MODAL);
+//        FXMLLoader loader = new FXMLLoader();
+//        Parent root = fxWeaver.loadView(AuthorizationController.class);
+//        Scene scene = new Scene(root);
+//        stageAuthorization.setScene(scene);
+//        stageAuthorization.show();
+    }
 }
